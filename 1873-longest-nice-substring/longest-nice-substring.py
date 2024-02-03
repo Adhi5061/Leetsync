e8687b203bd3722eff729c1e7edeafc920492ada
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-02-03 10:27:49 +0530

    Time: 27 ms (99.60%) | Memory: 16.6 MB (64.98%) - LeetSync

---
+++ 1873-longest-nice-substring/longest-nice-substring.py
@@ -0,0 +1,17 @@
+class Solution:
+    def longestNiceSubstring(self, s: str) -> str:
+        if(len(s)<2):
+            return ""
+        ss=set(s)
+        t=1
+        for ind,char in enumerate(s):
+            if(char.swapcase() in s):
+                continue
+            else:
+                t=0
+                break
+        if(t==1):
+            return s
+        else:
+            return(max(self.longestNiceSubstring(s[0:ind]),self.longestNiceSubstring(s[ind+1:]),key=len
+            ))
