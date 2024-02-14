b6e47be6caac82d37e8e81443f053360aefd4911
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-02-14 20:55:15 +0530

    Time: 38 ms (89.31%) | Memory: 16.6 MB (56.78%) - LeetSync

--- 31-next-permutation/next-permutation.py
+++ 31-next-permutation/next-permutation.py
@@ -4,16 +4,38 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         
-        temp=nums
-        pos=len(nums)-1
-        while(pos>=0):
-            ele=nums[pos]
-            ind=len(nums)-1
-            while(ind>pos):
-                if(nums[ind]>nums[pos]):
-                    nums[pos],nums[ind]=nums[ind],nums[pos]
-                    nums[pos+1:]=sorted(nums[pos+1:])
-                    return 
-                ind-=1
-            pos-=1
-        return nums.sort()
+        # temp=nums
+        # pos=len(nums)-1
+        # while(pos>=0):
+        #     ele=nums[pos]
+        #     ind=len(nums)-1
+        #     while(ind>pos):
+        #         if(nums[ind]>nums[pos]):
+        #             nums[pos],nums[ind]=nums[ind],nums[pos]
+        #             nums[pos+1:]=sorted(nums[pos+1:])
+        #             return 
+        #         ind-=1
+        #     pos-=1
+        # return nums.sort()
+        ind=len(nums)-2
+        while(ind>-1):
+            if(nums[ind]<nums[ind+1]):
+                break
+            ind-=1
+
+
+        if(ind==-1):
+            nums.reverse()
+            return
+        ele=nums[ind]
+        cmax=max(nums)
+        print(ele,ind,cmax,nums)
+        for temp in range(ind+1,len(nums)):
+            print(nums,temp,nums[temp])
+            print(nums[temp]>ele,nums[temp]<=cmax)
+            if(nums[temp]>ele and nums[temp]<=cmax):
+                print("sdfg")
+                ci=temp
+                cmax=nums[temp]
+        nums[ci],nums[ind]=nums[ind],nums[ci]
+        nums[ind+1:]=nums[ind+1:][::-1]
