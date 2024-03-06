6678dbd8c8b77944114611385d65018a4a5cce42
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-03-06 20:53:08 +0530

    Time: 193 ms (67.80%) | Memory: 37.5 MB (50.16%) - LeetSync

---
+++ 494-target-sum/target-sum.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        memo={}
+        def tsum(target,pos):
+            if((pos,target) in memo):
+                return memo[(pos,target)]
+            if(pos==len(nums)):
+                if(target==0):
+                    return 1
+                else:
+                    return 0
+            c=0
+            c+=tsum(target+nums[pos],pos+1)
+            c+=tsum(target-nums[pos],pos+1)
+            memo[(pos,target)]=c
+            return c
+        return tsum(target,0)
+        
