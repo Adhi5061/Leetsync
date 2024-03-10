4344fab1e13e76ff5100384490948ad181aed381
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-03-10 17:28:10 +0530

    Time: 41 ms (28.82%) | Memory: 16.9 MB (45.11%) - LeetSync

--- 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
+++ 107-binary-tree-level-order-traversal-ii/binary-tree-level-order-traversal-ii.py
@@ -7,15 +7,32 @@
 class Solution:
     def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
         
+        #Reversing the normal bfs traversal
+        # res=[]
+        # def levelo(root,level):
+        #     if(root==None):
+        #         return 
+        #     if(level<len(res)):
+        #         res[level].append(root.val)
+        #     else:
+        #         res.append([root.val])
+        #     levelo(root.left,level+1)
+        #     levelo(root.right,level+1)
+        # levelo(root,0)
+        # return res[::-1]
+
+        if(root==None):
+            return []
         res=[]
-        def levelo(root,level):
-            if(root==None):
-                return 
-            if(level<len(res)):
-                res[level].append(root.val)
-            else:
-                res.append([root.val])
-            levelo(root.left,level+1)
-            levelo(root.right,level+1)
-        levelo(root,0)
-        return res[::-1]
+        q=[root]
+        while(q):
+            temp=[]
+            for i in range(len(q)):
+                curr=q.pop(0)
+                temp.append(curr.val)
+                if(curr.left):
+                    q.append(curr.left)
+                if(curr.right):
+                    q.append(curr.right)
+            res.insert(0,temp)
+        return res
