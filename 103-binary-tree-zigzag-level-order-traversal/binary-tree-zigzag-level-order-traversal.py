e0bf9c024bedf586554f6a1158604d7ff3b76421
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-03-10 21:15:26 +0530

    Time: 41 ms (24.02%) | Memory: 16.7 MB (71.32%) - LeetSync

--- 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
+++ 103-binary-tree-zigzag-level-order-traversal/binary-tree-zigzag-level-order-traversal.py
@@ -16,15 +16,14 @@ class Solution:
             temp=[]
             for i in range(len(q)):
                 curr=q.pop(0)
-                if(left==0):
-                    temp.append(curr.val)
-                else:
-                    temp.insert(0,curr.val)
+                temp.append(curr.val)
                 if(curr.left):
                     q.append(curr.left)
                 if(curr.right):
                     q.append(curr.right)
-               
+            if(left==0):
+                res.append(temp)
+            else:
+                res.append(temp[::-1])
             left=(left+1)%2
-            res.append(temp)
         return res
