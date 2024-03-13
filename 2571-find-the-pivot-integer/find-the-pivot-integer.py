2766faaa89b19eef9411259ce1f8fb90beef5890
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-03-13 07:39:09 +0530

    Time: 67 ms (30.10%) | Memory: 16.7 MB (23.99%) - LeetSync

---
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.py
@@ -0,0 +1,13 @@
+class Solution:
+    def pivotInteger(self, n: int) -> int:
+        sum1=(n*(n+1))//2
+        sum2=n
+        for i in range(n,0,-1):
+            print(i,sum1,sum2)
+            if(sum1==sum2):
+                return i
+            if(sum2>sum1):
+                return -1
+            sum1-=i
+            sum2+=i-1
+        return -1
