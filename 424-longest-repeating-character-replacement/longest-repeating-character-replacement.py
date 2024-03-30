4a5ed85666ca230c6f70b30b40f0b46734eee5bf
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-03-30 11:23:32 +0530

    Time: 129 ms (20.08%) | Memory: 16.6 MB (49.47%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -3,16 +3,19 @@ class Solution:
         l=0
         r=0
         res=0
+        csum=0
         d={}
         while(r<len(s)):
             if(s[r] in d):
                 d[s[r]]+=1
             else:
                 d[s[r]]=1
-            if(sum(d.values())-max(d.values())>k):
+            csum+=1
+            if(csum-max(d.values())>k):
                 d[s[l]]-=1
+                csum-=1
                 l+=1
-            if(sum(d.values())-max(d.values())<=k):
+            if(csum-max(d.values())<=k):
                 res=max(res,r-l+1)
             r+=1
         return res
