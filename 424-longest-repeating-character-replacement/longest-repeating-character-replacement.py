af2347888fd232b053819c2d340449dea98fbe0e
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-03-30 11:24:59 +0530

    Time: 87 ms (63.74%) | Memory: 16.8 MB (13.87%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -4,6 +4,7 @@ class Solution:
         r=0
         res=0
         csum=0
+        mv=0
         d={}
         while(r<len(s)):
             if(s[r] in d):
@@ -11,11 +12,12 @@ class Solution:
             else:
                 d[s[r]]=1
             csum+=1
-            if(csum-max(d.values())>k):
+            mv=max(mv,d[s[r]])
+            if(csum-mv>k):
                 d[s[l]]-=1
                 csum-=1
                 l+=1
-            if(csum-max(d.values())<=k):
+            if(csum-mv<=k):
                 res=max(res,r-l+1)
             r+=1
         return res
