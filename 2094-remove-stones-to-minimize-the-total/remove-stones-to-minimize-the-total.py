616a3fe42b9352156111e96809dd226867db4ddc
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-04-22 20:27:42 +0530

    Time: 1196 ms (95.21%) | Memory: 31.2 MB (28.65%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.py
@@ -5,8 +5,9 @@ class Solution:
         heap=[-i for i in piles]
         heapq.heapify(heap)
         for i in range(k):
-            t=-heapq.heappop(heap)
-            newv=math.ceil(t/2)
-            # print(t,newv)
-            heapq.heappush(heap,-newv)
+            heapq.heapreplace(heap,heap[0]//2)
+            # t=-heapq.heappop(heap)
+            # newv=math.ceil(t/2)
+            # # print(t,newv)
+            # heapq.heappush(heap,-newv)
         return -sum(heap)
