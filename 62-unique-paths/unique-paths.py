920cc5e466693c72f62f6404684fc473f1d4af9f
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-04-28 11:47:32 +0530

    Time: 43 ms (10.48%) | Memory: 16.7 MB (10.39%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,10 +1,10 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
 
-        table=[[0]*n]*m
-        table[0]=[1]*len(table[0])
-        for i in range(len(table)):
-            table[i][0]=1
+        table=[[1]*n]*m
+        # table[0]=[1]*len(table[0])
+        # for i in range(len(table)):
+        #     table[i][0]=1
         for i in range(1,len(table)):
             for j in range(1,len(table[0])):
                 table[i][j]=table[i-1][j]+table[i][j-1]
