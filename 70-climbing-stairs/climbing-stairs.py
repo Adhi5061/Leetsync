c537497d6a2b056b54a2a3f84bf3c56fe691cf56
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-01 13:39:31 +0530

    Time: 37 ms (38.54%) | Memory: 16.6 MB (5.02%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,10 +1,17 @@
 class Solution:
     def climbStairs(self, n: int,memo={}) -> int:
-        if(n<3):
-            memo[n]=n
-            return n
-        if(n in memo):
-            return memo[n]
-        res=(self.climbStairs(n-1)+self.climbStairs(n-2))
-        memo[n]=res
-        return res
+        # if(n<3):
+        #     memo[n]=n
+        #     return n
+        # if(n in memo):
+        #     return memo[n]
+        # res=(self.climbStairs(n-1)+self.climbStairs(n-2))
+        # memo[n]=res
+        # return res
+        table=[0]*(n+1)
+        table[0]=1
+        table[1]=1
+        for i in range(2,len(table)):
+            print(i)
+            table[i]=table[i-1]+table[i-2]
+        return table[n]
