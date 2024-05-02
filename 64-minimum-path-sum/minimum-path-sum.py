0ab16192d2eca67f425ca8fcc0e98d52fff8f36a
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-02 17:35:28 +0530

    Time: 97 ms (16.12%) | Memory: 18.6 MB (22.20%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,16 +1,29 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        memo={}
 
-        def dp(i,j):
-            if((i,j) in memo):
-                return memo[(i,j)]
-            if(i==len(grid)-1 and j==len(grid[0])-1):
-                memo[(i,j)]=grid[i][j]
-                return grid[i][j]
-            if(i>=len(grid) or j>=len(grid[0])):
-                memo[(i,j)]=10**5
-                return 10**5
-            memo[(i,j)]=grid[i][j]+min(dp(i+1,j),dp(i,j+1))
-            return memo[(i,j)]
-        return dp(0,0)
+        table=[[float("inf")]*len(grid[0]) for _ in range(len(grid))]
+        print(table)
+        table[0][0]=grid[0][0]
+        for i in range(0,len(grid)):
+            for j in range(0,len(grid[0])):
+                if(j<len(grid[0])-1):
+                    table[i][j+1]=min(table[i][j+1],table[i][j]+grid[i][j+1])
+                if(i<len(grid)-1):
+                    table[i+1][j]=min(table[i+1][j],table[i][j]+grid[i+1][j])
+        print(table)
+        return table[-1][-1]
+
+        # memo={}
+
+        # def dp(i,j):
+        #     if((i,j) in memo):
+        #         return memo[(i,j)]
+        #     if(i==len(grid)-1 and j==len(grid[0])-1):
+        #         memo[(i,j)]=grid[i][j]
+        #         return grid[i][j]
+        #     if(i>=len(grid) or j>=len(grid[0])):
+        #         memo[(i,j)]=10**5
+        #         return 10**5
+        #     memo[(i,j)]=grid[i][j]+min(dp(i+1,j),dp(i,j+1))
+        #     return memo[(i,j)]
+        # return dp(0,0)
