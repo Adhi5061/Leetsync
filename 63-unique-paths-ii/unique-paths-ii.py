7c354bf84bdd9ad00fe33560cf28d083c62fb446
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-02 17:45:11 +0530

    Time: 47 ms (35.15%) | Memory: 17 MB (11.36%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,23 +1,47 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         
-        d={}
-        m=len(obstacleGrid)-1
-        n=len(obstacleGrid[0])-1
-        print(m,n)
-        def path(x,y):
-            if((x,y) in d):
-                return d[(x,y)]
-            if(x>m or y>n):
-                return 0
-            if(obstacleGrid[x][y]==1):
-                d[(x,y)]=0
-                return 0
-            if(x==m and y==n):
-                d[(x,y)]=1
+
+
+        row=len(obstacleGrid)
+        col=len(obstacleGrid[0])
+
+        memo={}
+        def dp(i,j):
+            if((i,j) in memo):
+                return memo[(i,j)]
+            if(i==row-1 and j==col-1 and obstacleGrid[i][j]!=1):
+                memo[(i,j)]=1
                 return 1
-            else:
-                ans= (path(x+1,y)+path(x,y+1))
-                d[(x,y)]=ans
-                return ans
-        return path(0,0)
+            if(i>=row or j>=col):
+                memo[(i,j)]=0
+                return 0
+            if(obstacleGrid[i][j]==1):
+                memo[(i,j)]=0
+                return 0
+            ans=dp(i+1,j)+dp(i,j+1)
+            memo[(i,j)]=ans
+            return ans
+        return dp(0,0)
+
+
+        # d={}
+        # m=len(obstacleGrid)-1
+        # n=len(obstacleGrid[0])-1
+        # print(m,n)
+        # def path(x,y):
+        #     if((x,y) in d):
+        #         return d[(x,y)]
+        #     if(x>m or y>n):
+        #         return 0
+        #     if(obstacleGrid[x][y]==1):
+        #         d[(x,y)]=0
+        #         return 0
+        #     if(x==m and y==n):
+        #         d[(x,y)]=1
+        #         return 1
+        #     else:
+        #         ans= (path(x+1,y)+path(x,y+1))
+        #         d[(x,y)]=ans
+        #         return ans
+        # return path(0,0)
