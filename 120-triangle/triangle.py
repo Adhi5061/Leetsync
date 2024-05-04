487884ad3fc4d3e9c8de689f02167fa4f7af2096
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-04 20:36:49 +0530

    Time: 53 ms (91.80%) | Memory: 17.9 MB (22.45%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,14 +1,22 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         
-        memo={}
-        def dp(i,j):
-            if((i,j) in memo):
-                return memo[(i,j)]
-            if(i==len(triangle) or j>=len(triangle[i])):
-                return 0
-            ans=triangle[i][j]+min(dp(i+1,j),dp(i+1,j+1))
-            memo[(i,j)]=ans
-            return ans  
+        table=[[10**5] *len(triangle) for _ in range(len(triangle))]
+        table[0][0]=triangle[0][0]
+        for i in range(0,len(triangle)-1):
+            for j in range(0,len(triangle[i])):
+                table[i+1][j]=min(table[i+1][j],triangle[i+1][j]+table[i][j])
+                table[i+1][j+1]=min(table[i+1][j+1],table[i][j]+triangle[i+1][j+1])
+        # print()
+        return min(table[-1])
+        # memo={}
+        # def dp(i,j):
+        #     if((i,j) in memo):
+        #         return memo[(i,j)]
+        #     if(i==len(triangle) or j>=len(triangle[i])):
+        #         return 0
+        #     ans=triangle[i][j]+min(dp(i+1,j),dp(i+1,j+1))
+        #     memo[(i,j)]=ans
+        #     return ans  
 
-        return dp(0,0)
+        # return dp(0,0)
