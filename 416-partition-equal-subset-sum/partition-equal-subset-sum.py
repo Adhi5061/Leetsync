5f3f3417f9132a115435811e372f8f9fee84d433
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-04 21:19:44 +0530

    Time: 582 ms (60.61%) | Memory: 19.7 MB (49.15%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.py
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.py
@@ -1,24 +1,35 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
-        memo={}
-        def sub(target,i):
-            if((target,i) in memo):
-                return memo[(target,i)]
-            if(target==0):
-                memo[(target,i)]=True
-                return True
-            if(target<0 or i==len(nums)):
-                memo[(target,i)]=False
-                return False
-            if(sub(target,i+1)):
-                memo[(target,i)]=True
-                return True
-            if(sub(target-nums[i],i+1)):
-                memo[(target,i)]=True
-                return True
-            memo[(target,i)]=False
-            return False
+        # memo={}
+        # def sub(target,i):
+        #     if((target,i) in memo):
+        #         return memo[(target,i)]
+        #     if(target==0):
+        #         memo[(target,i)]=True
+        #         return True
+        #     if(target<0 or i==len(nums)):
+        #         memo[(target,i)]=False
+        #         return False
+        #     if(sub(target,i+1)):
+        #         memo[(target,i)]=True
+        #         return True
+        #     if(sub(target-nums[i],i+1)):
+        #         memo[(target,i)]=True
+        #         return True
+        #     memo[(target,i)]=False
+        #     return False
+
         if(sum(nums)%2!=0):
             return False
-        return sub(sum(nums)/2,0)
+
+        s=set()
+        s.add(0)
+        for i in range(0,len(nums)):
+            t=set()
+            for j in s:
+                t.add(nums[i]+j)
+            s.update(t)
+ 
+        
+        return sum(nums)/2 in s
