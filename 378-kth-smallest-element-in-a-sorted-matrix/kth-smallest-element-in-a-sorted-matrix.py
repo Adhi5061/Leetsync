e630f8a3b9015ec9c2fed1ab4e061ee4de6a959f
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-08 16:01:01 +0530

    Time: 159 ms (43.31%) | Memory: 21.3 MB (29.13%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.py
@@ -3,6 +3,9 @@ class Solution:
         res=[]
         for i in range(0,len(matrix)):
             for j in range(0,len(matrix[0])):
+                if(len(res)>k and -heap[0]<matrix[i][j]):
+                    # t=1
+                    break
                 heapq.heappush(res,-matrix[i][j])
                 if(len(res)>k):
                     heapq.heappop(res)
