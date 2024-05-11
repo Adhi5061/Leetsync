9de3ea9db5ae53dc53392fb2e5e997c91bcaaccf
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-12 00:23:10 +0530

    Time: 6125 ms (100.00%) | Memory: 17.7 MB (100.00%) - LeetSync

--- 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.py
+++ 3403-minimum-substring-partition-of-equal-character-frequency/minimum-substring-partition-of-equal-character-frequency.py
@@ -1,27 +1,26 @@
 class Solution:
     def minimumSubstringsInPartition(self, s: str) -> int:
+        
         memo={}
         def dp(ind):
             if(ind in memo):
                 return memo[ind]
+            d=dict()
+            # print("sf",d)
+            ans=10000
             if(ind==len(s)):
                 return 0
-            d={}
-            ans=10**5
             for i in range(ind,len(s)):
-                
                 d[s[i]]=d.get(s[i],0)+1
-                value=list(d.values())[0]
+                val=list(d.values())[0]
                 flag=True
-                # print(ind,i,d,ans)
-                for indf,val in d.items():
+                for indg,value in d.items():
                     if(val!=value):
                         flag=False
                         break
                 if(flag):
-                    p=dp(i+1)
-                    ans=min(ans,1+p)
-                # print("After",ind,ans)
+                    ans=min(ans,1+dp(i+1))
+            # print("answer",ans)
             memo[ind]=ans
             return ans
         return dp(0)
