e563949ea9fd7cfb7dd259c14e8705d9430a3de2
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-14 09:40:27 +0530

    Time: 462 ms (67.70%) | Memory: 35.2 MB (41.02%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.py
+++ 131-palindrome-partitioning/palindrome-partitioning.py
@@ -2,17 +2,27 @@ class Solution:
     def partition(self, s: str) -> List[List[str]]:
         
         res=[]
-        def palindrome(curr,i,arr,flag):
-            # print(curr,i,arr)
+        def palindrome(i,arr):
             if(i==len(s)):
-                if(flag):
-                    res.append(arr[:])
-                return
-            curr+=s[i]
-            if(curr==curr[::-1]):
-                arr.append(curr)
-                palindrome("",i+1,arr,1)
-                arr.pop()
-            palindrome(curr,i+1,arr,0)
-        palindrome("",0,[],0)
+                res.append(arr[:])
+            for j in range(i,len(s)):
+                if(s[i:j+1]==s[i:j+1][::-1]):
+                    arr.append(s[i:j+1])
+                    palindrome(j+1,arr)
+                    arr.pop()
+        palindrome(0,[])
         return res
+        # def palindrome(curr,i,arr,flag):
+        #     # print(curr,i,arr)
+        #     if(i==len(s)):
+        #         if(flag):
+        #             res.append(arr[:])
+        #         return
+        #     curr+=s[i]
+        #     if(curr==curr[::-1]):
+        #         arr.append(curr)
+        #         palindrome("",i+1,arr,1)
+        #         arr.pop()
+        #     palindrome(curr,i+1,arr,0)
+        # palindrome("",0,[],0)
+        # return res
