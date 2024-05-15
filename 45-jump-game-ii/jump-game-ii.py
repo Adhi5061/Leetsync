360447b55e8e03e7a87e409aa4ea2bb7824527f2
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-05-15 21:30:08 +0530

    Time: 3768 ms (15.07%) | Memory: 17.7 MB (25.24%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.py
+++ 45-jump-game-ii/jump-game-ii.py
@@ -1,20 +1,28 @@
 class Solution:
     def jump(self, nums: List[int]) -> int:
         
-        d={}
-        def dp(pos):
-            if(pos in d):
-                # print("hi")
-                return d[pos]
-            if(pos==len(nums)-1):
-                return 0
-            if(pos>=len(nums)):
-                return len(nums)
-            # print(pos)
-            ans=len(nums)
-            for i in range(0,nums[pos]):
-                ans=min(ans,1+dp(pos+i+1))
-            d[pos]=ans
-            return ans
+        dp=[float("inf") for i in range(0,len(nums))]
+        dp[0]=0
+        for i in range(0,len(nums)):
+            for j in range(i+1,i+nums[i]+1):
+                if(j>=len(nums)):
+                    break
+                dp[j]=min(dp[j],dp[i]+1)
+        return dp[-1]
+        # d={}
+        # def dp(pos):
+        #     if(pos in d):
+        #         # print("hi")
+        #         return d[pos]
+        #     if(pos>=len(nums)-1):
+        #         return 0
+        #     # if(pos>=len(nums)):
+        #     #     return len(nums)
+        #     # print(pos)
+        #     ans=len(nums)
+        #     for i in range(0,nums[pos]):
+        #         ans=min(ans,1+dp(pos+i+1))
+        #     d[pos]=ans
+        #     return ans
 
-        return dp(0)
+        # return dp(0)
