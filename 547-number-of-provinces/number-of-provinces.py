802135083ad9ac8c93d0d094bdc0202a0d1b0712
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-06-11 17:57:26 +0530

    Time: 187 ms (42.02%) | Memory: 17.5 MB (31.44%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -1,18 +1,30 @@
+from queue import Queue
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
         visited=[0]*len(isConnected)
 
-        def dfs(node):
-            if(visited[node]):
-                return
+        # def dfs(node):
+        #     if(visited[node]):
+        #         return
+        #     visited[node]=1
+        #     for i in range(0,len(isConnected[node])):
+        #         if(isConnected[node][i]==1):
+        #             dfs(i)
+        def bfs(node):
+            q=Queue()
             visited[node]=1
-            for i in range(0,len(isConnected[node])):
-                if(isConnected[node][i]==1):
-                    dfs(i)
+            q.put(node)
+            while(not q.empty()):
+                n=q.get()
+                print(n)
+                for i in range(0,len(isConnected)):
+                    if(i!=n and isConnected[n][i]==1 and visited[i]==0):
+                        visited[i]=1
+                        q.put(i)
 
         res=0
         for i in range(0,len(visited)):
             if(visited[i]==0):
-                dfs(i)
+                bfs(i)
                 res+=1
         return res
