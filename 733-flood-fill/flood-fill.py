2111a4e8a07e48a0a895e13359ea155ccddd6b4a
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-06-12 17:32:41 +0530

    Time: 65 ms (68.12%) | Memory: 16.6 MB (68.05%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,25 +1,32 @@
-from queue import Queue
 class Solution:
     def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
-        
-        x=[0,0,1,-1]
-        y=[1,-1,0,0]
-        start=image[sr][sc]
-        res=[row[:] for row in image]
-        visited=set()
-        def bfs():
-            q=Queue()
-            q.put((sr,sc))
-            while(not q.empty()):
-                # print(q)
-                curr=q.get()
-                res[curr[0]][curr[1]]=color
-                image[curr[0]][curr[1]]=-1
-                for i in range(4):
-                    cx=curr[0]+x[i]
-                    cy=curr[1]+y[i]
-                    if(cx>=0 and cy>=0 and cx<len(image)  and cy<len(image[0]) and image[cx][cy]!=-1  and image[cx][cy]==start):
-                        q.put((cx,cy))
-                        # visited.add((cx,cy))
-        bfs()
-        return res
+        m = len(image)
+        n = len(image[0])
+        oldColor = image[sr][sc]
+        if oldColor == color:
+            return image
+        queue = deque([(sr,sc)])
+        visited = {(sr,sc)}
+        image[sr][sc] = color
+        while queue:
+            for _ in range(len(queue)):
+                i, j = queue.popleft()
+                
+                if i-1 >= 0 and image[i-1][j] == oldColor and (i-1, j) not in visited:
+                    image[i-1][j] = color
+                    queue.append((i-1,j))
+                    visited.add((i-1,j))
+                if j-1 >= 0 and image[i][j-1] == oldColor and (i, j-1) not in visited:
+                    image[i][j-1] = color
+                    queue.append((i,j-1))
+                    visited.add((i,j-1))
+                if i+1 < m and image[i+1][j] == oldColor and (i+1, j) not in visited:
+                    image[i+1][j] = color
+                    queue.append((i+1,j))
+                    visited.add((i+1,j))
+                if j+1 < n and image[i][j+1] == oldColor and (i, j+1) not in visited:
+                    image[i][j+1] = color
+                    queue.append((i,j+1))
+                    visited.add((i,j+1))
+                
+        return image
