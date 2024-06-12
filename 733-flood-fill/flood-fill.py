3bf319fc5b2c4191715d65727edd0f60d2ca772a
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-06-12 17:35:41 +0530

    Time: 67 ms (55.13%) | Memory: 16.6 MB (92.50%) - LeetSync

--- 733-flood-fill/flood-fill.py
+++ 733-flood-fill/flood-fill.py
@@ -1,4 +1,4 @@
-from queue import Queue
+from collections import deque
 from typing import List
 
 class Solution:
@@ -6,25 +6,26 @@ class Solution:
         x = [0, 0, 1, -1]
         y = [1, -1, 0, 0]
         start = image[sr][sc]
-        res = [row[:] for row in image]
         
+        # If the starting color is the same as the new color, return the original image
         if start == color:
-            return res
+            return image
         
-        q = Queue()
-        q.put((sr, sc))
-        visited = set()
-        visited.add((sr, sc))
-        
-        while not q.empty():
-            curr = q.get()
-            res[curr[0]][curr[1]] = color
-            
-            for i in range(4):
-                cx = curr[0] + x[i]
-                cy = curr[1] + y[i]
-                if (0 <= cx < len(image) and 0 <= cy < len(image[0]) and
-                    (cx, cy) not in visited and image[cx][cy] == start):
-                    q.put((cx, cy))
-                    visited.add((cx, cy))
+        res = [row[:] for row in image]  # Make a deep copy of the original image
+
+        def bfs():
+            q = deque([(sr, sc)])
+            while q:
+                curr = q.popleft()
+                res[curr[0]][curr[1]] = color
+                image[curr[0]][curr[1]] = -1  # Mark the cell as visited by setting it to -1
+                
+                for i in range(4):
+                    cx = curr[0] + x[i]
+                    cy = curr[1] + y[i]
+                    if 0 <= cx < len(image) and 0 <= cy < len(image[0]) and image[cx][cy] == start:
+                        q.append((cx, cy))
+                        image[cx][cy] = -1  # Mark the cell as visited
+
+        bfs()
         return res
