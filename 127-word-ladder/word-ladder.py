a037947be833f0069cf56fbbd3f932a66ab693b1
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-16 11:19:06 +0530

    Time: 369 ms (41.44%) | Memory: 18.1 MB (63.18%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -6,27 +6,20 @@ class Solution:
         wordList=set(wordList)
         if(beginWord in wordList):
             wordList.remove(beginWord)
-            t+=1
         q.append((beginWord,1))
         visited=set()
         while(q):
             word,l=q.popleft()
-            # print(word,l,visited)
             if(len(visited)==len(wordList)):
                 return 0
-            for wordl in wordList:
-                if(wordl in visited):
-                    continue
-                if(len(wordl)==len(word)):
-                    res=0
-                    for i in range(len(word)):
-                        res+=1 if(word[i]!=wordl[i]) else 0
-                    # print(wordl,word,res)
-                    if(res==1):
+            for i in range(len(word)):
+                for char in "abcdefghijklmnopqrstuvwxyz":
+                    wordl=word[:i]+char+word[i+1:]
+                    if(wordl in wordList):
+                        if(wordl in visited):
+                            continue
                         if(wordl==endWord):
                             return l+1
                         visited.add(wordl)
-                        # print(elist)
-                        # print(word,wordl,l+1)
                         q.append((wordl,l+1))
         return 0
