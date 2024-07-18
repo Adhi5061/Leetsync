57a0db9f9c1259e42a35ca667d46b32dc5c6e675
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-18 20:48:14 +0530

    Time: 80 ms (94.59%) | Memory: 18.3 MB (41.05%) - LeetSync

---
+++ 207-course-schedule/course-schedule.py
@@ -0,0 +1,23 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        from collections import deque
+        q=deque()
+        indeg=[0]*numCourses
+        adj=[[] for row in range(numCourses)]
+        for after,prev in prerequisites:
+            adj[prev].append(after)
+        print(adj)
+        for row in prerequisites:
+            indeg[row[0]]+=1
+        for ind in range(numCourses):
+            if(indeg[ind]==0):
+                q.append(ind)
+        res=[]
+        while(q):
+            course=q.popleft()
+            res.append(course)
+            for node in adj[course]:
+                indeg[node]-=1
+                if(indeg[node]==0):
+                    q.append(node)
+        return len(res)==numCourses
