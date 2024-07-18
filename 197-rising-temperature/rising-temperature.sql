7e5f993109c083ba7014ed5f7c98bce043524e11
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-18 16:11:36 +0530

    Time: 439 ms (65.11%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -2,6 +2,9 @@
 -- select id from weather A 
 -- where A.temperature>(select temperature from weather B
 -- where B.recordDate=A.recordDate-INTERVAL 1 DAY);
-select w2.id as Id from weather w1 left join 
-weather w2 on w1.recordDate=w2.recordDate-Interval 1 day
-where w2.temperature>w1.temperature;
+-- select w2.id as Id from weather w1 left join 
+-- weather w2 on w1.recordDate=w2.recordDate-Interval 1 day
+-- where w2.temperature>w1.temperature;
+select w2.id from weather w1 join weather w2
+where Datediff(w2.recordDate,w1.recordDate)=1
+and w2.temperature>w1.temperature;
