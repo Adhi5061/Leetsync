b268ba760ef4272322ba61461a3b7082f5a9ecb7
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-21 18:38:14 +0530

    Time: 31 ms (85.20%) | Memory: 16.6 MB (22.81%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -11,13 +11,3 @@ class Solution:
             prev1=curr
             
         return prev1
-        # memo={0:nums[0]}
-        # def dp(n):
-        #     if(n<0):
-        #         return 0
-        #     if(n in memo):
-        #         return memo[n]
-        #     ans= max(nums[n]+dp(n-2),dp(n-1))
-        #     memo[n]=ans
-        #     return ans
-        # return dp(len(nums)-1)
