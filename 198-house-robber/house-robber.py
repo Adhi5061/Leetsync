326a85b5872a370aca6456f19111052ec51ef6cd
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-21 18:30:07 +0530

    Time: 36 ms (55.05%) | Memory: 16.5 MB (62.27%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,27 +1,13 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if(len(nums)==1):
-            return nums[0]
-        table=[0]*len(nums)
-        table[0]=nums[0]
-        table[1]=nums[1]
-        for i in range(0,len(nums)):
-            for j in range(i+2,len(nums)):
-                table[j]=max(table[j],table[i]+nums[j])
-            # print(table)
-        return max(table)
         
-        
-        # memo={}
-        # def rob(arr,pos):
-        #     if(len(arr)<=pos):
-        #         memo[pos]=0
-        #         return 0
-        #     if(len(arr)-pos==1):
-        #         memo[pos]=arr[pos]
-        #         return arr[pos]
-        #     if(pos in memo):
-        #         return memo[pos]
-        #     memo[pos]= max(arr[pos]+rob(arr,pos+2),arr[pos+1]+rob(arr,pos+3))
-        #     return memo[pos]
-        # return rob(nums,0)
+        memo={0:nums[0]}
+        def dp(n):
+            if(n<0):
+                return 0
+            if(n in memo):
+                return memo[n]
+            ans= max(nums[n]+dp(n-2),dp(n-1))
+            memo[n]=ans
+            return ans
+        return dp(len(nums)-1)
