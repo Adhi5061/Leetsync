b66b0d2c9e46f3cd95a01a54cbb0e759aa0e4f16
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-22 18:04:30 +0530

    Time: 40 ms (25.04%) | Memory: 16.8 MB (7.90%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,25 +1,14 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-
-        table=[[1]*n]*m
-        # table[0]=[1]*len(table[0])
-        # for i in range(len(table)):
-        #     table[i][0]=1
-        for i in range(1,len(table)):
-            for j in range(1,len(table[0])):
-                table[i][j]=table[i-1][j]+table[i][j-1]
-        return table[m-1][n-1]
-        
-
-        # d={}
-        # def dp(m,n):
-        #     if (m,n) in d:
-        #         return d[(m,n)]
-        #     if(m<1 or n<1):
-        #         return 0
-        #     if(m==1 and n==1):
-        #         return 1
-        #     ans=dp(m-1,n)+dp(m,n-1)
-        #     d[(m,n)]=ans
-        #     return ans
-        # return dp(m,n)
+        memo={}
+        def dp(i,j):
+            if((i,j) in memo):
+                return memo[(i,j)]
+            if(i==0 and j==0):
+                return 1
+            if(i<0 or j<0):
+                return 0
+            ans=dp(i-1,j)+dp(i,j-1)
+            memo[(i,j)]=ans
+            return ans
+        return dp(m-1,n-1)
