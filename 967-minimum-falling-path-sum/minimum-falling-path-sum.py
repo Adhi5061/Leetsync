9779cf163dc3c5eb97ebc5323a13feca893a9071
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-24 00:21:12 +0530

    Time: 117 ms (91.14%) | Memory: 17.3 MB (84.15%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,22 +1,37 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        memo={}
-        def dp(i,j):
-            if(i<0 or j<0 or j>=len(matrix[0])):
-                return 10**9
-            # print(i,j)
-            if((i,j) in memo):
-                return memo[(i,j)]
-            minv=10**9
-            minv=min(minv,dp(i-1,j),dp(i-1,j-1),dp(i-1,j+1))
-            if(minv==10**9):
-                minv=0
-            # print(minv,end=" ")
-            minv+=matrix[i][j]
-            memo[(i,j)]=minv
-            # print(i,j,minv)
-            return minv
-        res=10**9
+        
+        dp=[[float("inf")]*len(matrix[0]) for _ in range(len(matrix))]
         for i in range(0,len(matrix[0])):
-            res=min(res,dp(len(matrix)-1,i))
-        return res
+            dp[0][i]=matrix[0][i]
+        for i in range(1,len(matrix)):
+            for j in range(0,len(matrix[0])):
+                if(j>0 and j<len(matrix[0])-1):
+                    dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j-1],dp[i-1][j+1])
+                else:
+                    if(j>0):
+                        dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j-1])
+                    else:
+                        dp[i][j]=matrix[i][j]+min(dp[i-1][j],dp[i-1][j+1])
+        # print(dp)
+        return min(dp[-1])
+        # memo={}
+        # def dp(i,j):
+        #     if(i<0 or j<0 or j>=len(matrix[0])):
+        #         return 10**9
+        #     # print(i,j)
+        #     if((i,j) in memo):
+        #         return memo[(i,j)]
+        #     minv=10**9
+        #     minv=min(minv,dp(i-1,j),dp(i-1,j-1),dp(i-1,j+1))
+        #     if(minv==10**9):
+        #         minv=0
+        #     # print(minv,end=" ")
+        #     minv+=matrix[i][j]
+        #     memo[(i,j)]=minv
+        #     # print(i,j,minv)
+        #     return minv
+        # res=10**9
+        # for i in range(0,len(matrix[0])):
+        #     res=min(res,dp(len(matrix)-1,i))
+        # return res
