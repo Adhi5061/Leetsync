d87bf8875a2e98b31527e180db4fc939b528f06c
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-24 00:14:41 +0530

    Time: 151 ms (25.49%) | Memory: 26.5 MB (6.57%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,19 +1,22 @@
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
-        
         memo={}
         def dp(i,j):
+            if(i<0 or j<0 or j>=len(matrix[0])):
+                return 10**9
+            # print(i,j)
             if((i,j) in memo):
                 return memo[(i,j)]
-            if(i<0 or j<0 or i>len(matrix) or j>=len(matrix)):
-                return 10**5
-            if(i==len(matrix)):
-                return 0
-            ans=matrix[i][j]+min(dp(i+1,j),dp(i+1,j-1),dp(i+1,j+1))
-            memo[(i,j)]=ans
-            return ans
-
-        ans=10**5
-        for i in range(0,len(matrix)):
-            ans=min(ans,dp(0,i))
-        return ans
+            minv=10**9
+            minv=min(minv,dp(i-1,j),dp(i-1,j-1),dp(i-1,j+1))
+            if(minv==10**9):
+                minv=0
+            # print(minv,end=" ")
+            minv+=matrix[i][j]
+            memo[(i,j)]=minv
+            # print(i,j,minv)
+            return minv
+        res=10**9
+        for i in range(0,len(matrix[0])):
+            res=min(res,dp(len(matrix)-1,i))
+        return res
