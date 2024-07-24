9a4a491a6115a362c8c6a310e228e4cbd70b156c
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-24 20:09:43 +0530

    Time: 31 ms (96.17%) | Memory: 17.7 MB (27.62%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,48 +6,9 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-
-        #Recursive Solution 
-
-        # if(root==None):
-        #     return 0
-        # l=self.maxDepth(root.left)
-        # r=self.maxDepth(root.right)
-        # return(max(l,r)+1)
-        # if(root==None):
-        #     return 0
-        # l=self.maxDepth(root.left)
-        # r=self.maxDepth(root.right)
-        # return max(l,r)+1
-
-        #While loop 
-
-        # q=[(root,1)]
-        # if(root==None):
-        #     return 0
-        # curr=None
-        # maxv=0
-        # while(q):
-        #     curr,ind=q.pop(0)
-        #     maxv=max(maxv,ind)
-        #     if(curr.left):
-        #         q.append([curr.left,ind+1])
-        #     if(curr.right):
-        #         q.append([curr.right,ind+1])
-        # return maxv
-
-        #For Loop
-
-        if(root==None):
-            return 0
-        q=[root]
-        level=0
-        while(q):
-            for i  in range(len(q)):
-                curr=q.pop(0)
-                if(curr.left):
-                    q.append(curr.left)
-                if(curr.right):
-                    q.append(curr.right)
-            level+=1
-        return level
+        
+        def depth(root):
+            if(root==None):
+                return 0
+            return 1+max(depth(root.left),depth(root.right))
+        return depth(root)
