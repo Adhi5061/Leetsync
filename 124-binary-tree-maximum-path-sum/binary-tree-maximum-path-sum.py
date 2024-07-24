9bf11cdc1ef3762d45ee2878d6c9cb0002ad7e36
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-24 21:20:39 +0530

    Time: 74 ms (12.68%) | Memory: 20.9 MB (42.17%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.py
@@ -6,16 +6,26 @@
 #         self.right = right
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
-        self.cmax=-10000
-        def dfs(root):
+        res=[-float("inf")]
+        def maxv(root):
             if(root==None):
                 return 0
-            l=dfs(root.left)
-            r=dfs(root.right)
-            cval=max(l+r+root.val,l+root.val,r+root.val,root.val)
-            self.cmax=max(self.cmax,cval)
-            return max(l+root.val,r+root.val,root.val)
-        dfs(root)
-        return self.cmax
+            left=maxv(root.left)
+            right=maxv(root.right)
+            res[0]=max(res[0],root.val+left+right,root.val,root.val+max(left,right))
+            return max(root.val,root.val+max(left,right))
+        maxv(root)
+        return res[0]
+        # self.cmax=-10000
+        # def dfs(root):
+        #     if(root==None):
+        #         return 0
+        #     l=dfs(root.left)
+        #     r=dfs(root.right)
+        #     cval=max(l+r+root.val,l+root.val,r+root.val,root.val)
+        #     self.cmax=max(self.cmax,cval)
+        #     return max(l+root.val,r+root.val,root.val)
+        # dfs(root)
+        # return self.cmax
 
         
