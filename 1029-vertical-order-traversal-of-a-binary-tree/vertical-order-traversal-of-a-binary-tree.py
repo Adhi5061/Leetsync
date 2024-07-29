3fb7ac578041eb17b2ff6277ba34b720684cb092
Adhi5061 <120810982+Adhi5061@users.noreply.github.com>
2024-07-29 07:42:10 +0530

    Time: 23 ms (99.65%) | Memory: 16.7 MB (44.28%) - LeetSync

--- 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
+++ 1029-vertical-order-traversal-of-a-binary-tree/vertical-order-traversal-of-a-binary-tree.py
@@ -6,29 +6,34 @@
 #         self.right = right
 class Solution:
     def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        from collections import deque
+        q=deque()
+        q.append((root,0))
         d={}
-
-        def dfs(root,level,depth):
-            if(root==None):
-                return
-            temp=[]
-            if(level in d):
-                dic=d[level]
-                if(depth in dic):
-                    dic[depth].append(root.val)
+        depth=0
+        while(q):
+            for t in range(len(q)):
+                node,level=q.popleft()
+                if(level in d):
+                    if(depth in d[level]):
+                        d[level][depth].append(node.val)
+                    else:
+                        d[level][depth]=[node.val]
                 else:
-                    dic[depth]=[root.val]
-            else:
-                d[level]={depth:[root.val]}
-            dfs(root.left,level-1,depth+1)
-            dfs(root.right,level+1,depth+1)
-        dfs(root,0,0)
-        d=dict(sorted(d.items(),key=lambda x:(x[0],x[1])))
+                    d[level]={}
+                    d[level][depth]=[node.val]
+                if(node.left):
+                    q.append((node.left,level-1))
+                if(node.right):
+                    q.append((node.right,level+1))
+            depth+=1
+        # print(d)
+        d=dict(sorted(d.items()))
         res=[]
-        for dicti in d.values():
+        # print(d)
+        for key,val in d.items():
             temp=[]
-            dicti=dict(sorted(dicti.items()))
-            for i in dicti:
-                temp.extend(sorted(dicti[i]))
+            for key2,val2 in val.items():
+                temp.extend(sorted(val2))
             res.append(temp)
         return res
